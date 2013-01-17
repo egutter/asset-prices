29ad549e857d49692f9f2e5cb750b166813f072c
Emilio Gutter <egutter@10pines.com>
2013-01-16 23:55:09 -0300

    Do not import values if series already exists at parsed date

--- app/tempo_db_repository.rb
+++ app/tempo_db_repository.rb
@@ -2,12 +2,12 @@ require 'tempodb'
 
 class TempoDbRepository
 
-  def initialize
-    api_key = ENV['TEMPODB_API_KEY']
-    api_secret = ENV['TEMPODB_API_SECRET']
-    api_host = ENV['TEMPODB_API_HOST']
-    api_port = Integer(ENV['TEMPODB_API_PORT'])
-    api_secure = ENV['TEMPODB_API_SECURE'] == "False" ? false : true
+  def initialize(config = {})
+    api_key = config[:api_key] || ENV['TEMPODB_API_KEY']
+    api_secret = config[:api_secret] || ENV['TEMPODB_API_SECRET']
+    api_host = config[:api_host] || ENV['TEMPODB_API_HOST']
+    api_port = Integer(config[:api_port] || ENV['TEMPODB_API_PORT'])
+    api_secure = (config[:api_secure] || ENV['TEMPODB_API_SECURE']) == "False" ? false : true
 
     @client = TempoDB::Client.new( api_key, api_secret, api_host, api_port, api_secure )
   end
@@ -23,6 +23,14 @@ class TempoDbRepository
   
   def commit
     @client.write_bulk(@data_point_ts, @records) 
+  end
+
+  def has_values_at?(ts)
+    !read_at(ts).map(&:data).flatten.empty?
+  end
+
+  def read_at(ts)
+    @client.read(Time.utc(ts.year, ts.month, ts.day, 0, 0, 0), Time.utc(ts.year, ts.month, ts.day, 23, 59, 59))
   end
 end
 
